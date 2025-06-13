4b1c5368bb8ddb9a0336b4beb4051ca7729cd1f4
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-06-13 08:01:29 -0300

    3o exercício de loop - while

---
+++ Loops/loop3-while.py
@@ -0,0 +1,7 @@
+print("Exibindo números limitados com while \n")
+
+cont = 1
+
+while cont <= 5: # enquanto cont for menor ou igual a 5, exibir número na tela
+    print(cont)
+    cont += 1 # incrementação
